c7131c8f558de888385996381380a56c524a3853
Philip Emanuele <emanpa68@gmail.com>
2024-09-11 21:35:37 -0700

    Initial Commit

---
+++ BatteryManager/BatteryManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryManagerApp.swift
+//  BatteryManager
+//
+//  Created by Philip Emanuele on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BatteryManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
